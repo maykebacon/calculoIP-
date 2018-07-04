0874cc75df2a9bd9e8029f05e3aefc93d95e076e
Mayke brayan <maykebrayan88@gmail.com>
2018-07-04 14:02:13 -0300

    Add files via upload

---
+++ calc_ipv4/app/classes/class_calc_ip.php
@@ -0,0 +1,119 @@
+<?php
+
+class calc_ip
+{
+    public $endereco;
+    public $cidr;
+    public $endereco_completo;
+
+    public function __construct( $endereco_completo ) {
+        $this->endereco_completo = $endereco_completo;
+        $this->valida_endereco();
+    }
+
+    public function valida_endereco() {
+
+        $regexp = '/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$/';
+
+        if ( ! preg_match( $regexp, $this->endereco_completo ) ) {
+            return false;
+        }
+        $endereco = explode( '/', $this->endereco_completo );
+        $this->cidr = (int) $endereco[1];
+        $this->endereco = $endereco[0];
+        if ( $this->cidr > 32 ) {
+            return false;
+        }
+        foreach( explode( '.', $this->endereco ) as $numero ) {
+            $numero = (int) $numero;
+            if ( $numero > 255 || $numero < 0 ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function endereco_completo() { 
+        return ( $this->endereco_completo ); 
+    }
+
+    public function endereco() { 
+        return ( $this->endereco ); 
+    }
+
+    public function cidr() { 
+        return ( $this->cidr ); 
+    }
+
+    public function mascara() {
+        if ( $this->cidr() == 0 ) {
+            return '0.0.0.0';
+        }
+
+        return ( 
+            long2ip(
+                ip2long("255.255.255.255") << ( 32 - $this->cidr ) 
+            )
+        );
+    }
+    public function rede() {
+        if ( $this->cidr() == 0 ) {
+            return '0.0.0.0';
+        }
+
+        return (
+            long2ip(
+                ( ip2long( $this->endereco ) ) & ( ip2long( $this->mascara() ) )
+            )
+        );
+    }
+    public function broadcast() {
+        if ( $this->cidr() == 0 ) {
+            return '255.255.255.255';
+        }
+        
+        return (
+            long2ip( ip2long($this->rede() ) | ( ~ ( ip2long( $this->mascara() ) ) ) )
+        );
+    }
+    public function total_ips() {
+        return( pow(2, ( 32 - $this->cidr() ) ) );
+    }
+
+    public function ips_rede() {
+        if ( $this->cidr() == 32 ) {
+            return 0;
+        } elseif ( $this->cidr() == 31 ) {
+            return 0;
+        }
+        
+        return( abs( $this->total_ips() - 2 ) );
+    }
+    public function primeiro_ip() {
+        if ( $this->cidr() == 32 ) {
+            return null;
+        } elseif ( $this->cidr() == 31 ) {
+            return null;
+        } elseif ( $this->cidr() == 0 ) {
+            return '0.0.0.1';
+        }
+        
+        return (
+            long2ip( ip2long( $this->rede() ) | 1 )
+        );
+    }
+
+    public function ultimo_ip() {
+        if ( $this->cidr() == 32 ) {
+            return null;
+        } elseif ( $this->cidr() == 31 ) {
+            return null;
+        }
+    
+        return (
+            long2ip( ip2long( $this->rede() ) | ( ( ~ ( ip2long( $this->mascara() ) ) ) - 1 ) )
+        );
+    }
+}
+
---
+++ calc_ipv4/index.php
@@ -0,0 +1,52 @@
+<?php include 'app/classes/class_calc_ip.php'; ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="../assets/css.css">
+
+    <meta charset="UTF-8">
+    
+    <title>Cálculo de máscara de sub-rede IPv4</title>
+
+</head>
+<body>
+    <section>
+        <h1>Calcular máscara de sub-rede IPv4</h1>
+    
+    <form method="POST">
+        <b style="color: green">IP/CIDR</b> <small>(Ex.: 192.168.0.1/24)</small> <br> 
+        <input style="border:1px solid green; line-height: 2; padding: 0 5px; width: 200px;" type="text" name="ip" value="<?php echo @$_POST['ip'];?>">
+        <input style="border:1px solid green; background: green; color: #fff; font-weight: 700; cursor: pointer; line-height: 2; padding: 0 5px;" type="submit" value="Calcular">
+    </form>
+   
+<?php
+if ( $_SERVER['REQUEST_METHOD'] === 'POST' && ! empty( $_POST['ip'] ) ) {
+    $ip = new calc_ip( $_POST['ip'] );
+    
+    if( $ip->valida_endereco() ) {
+        echo '<h2>Configurações de rede para <span style="color: darkslategray;">' . $_POST['ip'] . '</span> </h2>';
+        echo "<pre class='resultado'>";
+        
+        echo "<b>Endereço/Rede: </b>" . $ip->endereco_completo() . '<br>';
+        echo "<b>Endereço: </b>" . $ip->endereco() . '<br>';
+        echo "<b>Prefixo CIDR: </b>/" . $ip->cidr() . '<br>';
+        echo "<b>Máscara de sub-rede: </b>" . $ip->mascara() . '<br>';
+        echo "<b>IP da Rede: </b>" . $ip->rede() . '/' . $ip->cidr() . '<br>';
+        echo "<b>Broadcast da Rede: </b>" . $ip->broadcast() . '<br>';
+        echo "<b>Primeiro Host: </b>" . $ip->primeiro_ip() . '<br>';
+        echo "<b>Último Host: </b>" . $ip->ultimo_ip() . '<br>';
+        echo "<b>Total de IPs:  </b>" . $ip->total_ips() . '<br>';
+        echo "<b>Hosts: </b>" . $ip->ips_rede();
+        echo "</pre>";
+    } else {
+        echo 'Endereço IPv4 inválido!';
+    }
+}
+?>
+   </section>
+
+</body>
+</html>
+
